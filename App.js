05cba1006f70c4128c582c39a7040469ee299713
suleman510 <bsef17m510@pucit.edu.pk>
2020-12-17 01:07:00 +0500

    Using State

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name,setName]=useState('Suleman')
+  const [person, setPerson] = useState({ name: 'Aleem', age: 50})
+  
+  const pressHandler = () => {
+    setName('M Suleman Butt')
+    setPerson({name:'Dar',age:35})
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello Suleman!!!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>Lorem ipsum <Text>Pakistan</Text> dolloar sit amet.</Text>
-        <Text style={styles.boldText}>Lorem ipsum <Text>India</Text> dolloar sit amet.</Text>
-        <Text style={styles.boldText}>Lorem ipsum <Text>USA</Text> dolloar sit amet.</Text>
-      </View>
+        <Text>My name is {name}.</Text>
+        <Text>His name is {person.name} and his age is {person.age} years.</Text>
+        <View style={styles.buttonContainer}>
+          <Button title='Update State' onPress={pressHandler}/>
+        </View>
     </View>
   );
 }
@@ -24,16 +30,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header:{
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText:{
-    fontWeight: 'bold',
-
-  },
-  body:{
-    backgroundColor:'yellow',
-    padding: 20,
-  },
+  buttonContainer:{
+    marginTop: 20
+  }
 });
