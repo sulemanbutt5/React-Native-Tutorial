9fae397c522de9664fb458185f0baa6f35e4fce1
suleman510 <bsef17m510@pucit.edu.pk>
2020-12-17 00:54:45 +0500

    Views, Text and Styles

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Suleman Butt React Native App</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello Suleman!!!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum <Text>Pakistan</Text> dolloar sit amet.</Text>
+        <Text style={styles.boldText}>Lorem ipsum <Text>India</Text> dolloar sit amet.</Text>
+        <Text style={styles.boldText}>Lorem ipsum <Text>USA</Text> dolloar sit amet.</Text>
+      </View>
     </View>
   );
 }
@@ -17,5 +23,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header:{
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText:{
+    fontWeight: 'bold',
+
+  },
+  body:{
+    backgroundColor:'yellow',
+    padding: 20,
   },
 });
